811959a0a51f177ad6e443d11f56d8e1d3fd56b4
Alessio Vierti <alessio.vierti@gmail.com>
2018-05-01 11:33:17 +0200

    Pulizia codice e aggiunta figura al punto 3
    Aggiunta figura per il confronto delle singole componenti al punto 3.
    Rimosso il controllo se il sistema sia stabile, perchè non richiesto.
    Pulizia generale del codice (formattazione, rimozione commenti ridondanti, aggiunta commenti eventualmente utili).

--- main3.m
+++ main3.m
@@ -1,77 +1,86 @@
 %PARTE 3 PROGETTO MTFIA
 
-%Reset del workspace:
 clc;
 clear;
 
-sample_time = 1; %giorno
-horizon = 7;     %giorni
+
+% DESCRIZIONE
+% Si assume che lo stato sia perfettamente osservabile. Si vuole definire
+% il controllo ottimo in modo da minimizzare l'errore quadratico dello
+% stato e del controllo rispetto al seguente segnale, con time horizon di 7
+% giorni:
+% x1(k) ~ 10, k = 3, 4
+% x2(k) ~ -10, per ogni k
+% x3(k) ~ 0, k = 5
+
+
+% DEFINIZIONE PARAMETRI
+sample_time = 1;
+horizon = 7;
 interval = 1:sample_time:horizon;
 T = length(interval);
 
-%Definizione matrici A,B,C,D:
 A = eye(3); 
-B = [2 0 -1 0 0; 0 1 0 -2 0; 0 0 1 3 -1]; %alfa
+B = [2 0 -1 0 0; 0 1 0 -2 0; 0 0 1 3 -1];
 C = eye(3);
 D = zeros(3,5);
 
-%Definizione stato iniziale X0:
 X0 = [100, 70, -45]';
 
-%Punto 3)
-%     Sotto le ipotesi del punto 1) (stato osservaile ed orizzonte di 7 giorni)
-%     si vuole minimizzare il costo dello stato e del controllo rispettando
-%     i vincoli:  x1(3) = 10
-%                 x1(4) = 10
-%                 x2(t) = -10 per ogni t
-%                 x3(5) = 0
 
-%Definizione parametri per le matrici dei costi:
-gamma = 3; %Quanto vogliamo il riferimento a 0 degli stati
-eta = 2.26; %Per rendere la matrice R semidefinita positiva
-q = 7; %Quanto 'costa' lo stato
-r = 1; %Quanto 'costa' il controllo
-
-%Definizione matrici costi quadratici:
-Q = eye(3);
-Q(2,1) = -1;
-Q(1,2) = -1;
-Q(3,3) = 0;
-Q = q*(Q + gamma*eye(3));
+% DEFINIZIONE MATRICI Q E R
+q = 7;      % Definisce il peso (costo) dello stato.
+r = 1;      % Definisce il peso (costo) del controllo.
+gamma = 3;  % Peso per portare a zero lo stato.
+eta = 2.26; % Peso per portare a zero il controllo.
+Q = q * ([1 -1 0; -1 1 0; 0 0 0] + gamma * eye(3));
 Qf = Q;
+R = r * ([16 0 -4 0 0; 0 0 0 0 0; -4 0 1 0 0; 0 0 0 1 -2; 0 0 0 -2 4] + eta * eye(5));
 
-R = [16 0 -4 0 0; 0 0 0 0 0; -4 0 1 0 0; 0 0 0 1 -2; 0 0 0 -2 4];
-R = r*(R + eta*eye(5));
 
-%Definizione matrice del tracking:
-Z = zeros(size(X0,1),T);
-Z(2,:) = -10;
-Z(1,3:4) = 10;
-Z(3,5) = 0;
+% DEFINIZIONE SEGNALE DA TRACCIARE
+Z = zeros(size(X0,1), T);
+Z(1,3:4) = 10;              % x1(k) ~ 10, k = 3, 4
+Z(2,:) = -10;               % x2(k) ~ -10, per ogni k
+Z(3,5) = 0;                 % x3(k) ~ 0, k = 5
 
-%Ottengo il valore di Kp:
+
+% CALCOLO DI COMPONENTE PROPORZIONALE E INTEGRALE
 [Kp ,Ki, g] = mylqt(A, B, C, Q, Qf, R, T, Z, X0);
 
-%Simulo il sistema con il controllo ottimo:
+
+% SIMULAZIONE E PLOT DEL SISTEMA
 X = zeros(size(X0,1), T);
 U = zeros(size(B,2), T);
 Y = zeros(size(C,1), T);
 X(:,1) = X0;
 
 for k = 1:T-1
-    U(:,k) = -Kp(:,:,k)*X(:,k) + Ki(:,:,k)*g(:,k+1);
-    X(:,k+1) = A*X(:,k) + B*U(:,k);
-    Y(:,k) = C*X(:,k);
+    % Calcolo del controllo ottimo:
+    U(:,k) = -Kp(:,:,k) * X(:,k) + Ki(:,:,k) * g(:,k+1);
+    X(:,k+1) = A * X(:,k) + B * U(:,k);
+    Y(:,k) = C * X(:,k);
 end
 
-%Plot della simulazione:
-subplot (2,1,1);
-plot(interval,Y, interval, Z);
-title('CONTROLLO STATO LQR');
-legend('x1','x2','x3','z1','z2','z3');
-
-%Plot del controllo:
-subplot (2,1,2);
-plot(interval,U);
-title('CONTROLLI LQR');
-legend('u1','u2','u3', 'u4', 'u5');
+figure(1)
+subplot(2, 1, 1);
+plot(interval, X, interval, Z);
+title('Confronto stato e segnale da tracciare');
+legend('x1', 'x2', 'x3', 'z1', 'z2', 'z3');
+subplot(2, 1, 2);
+plot(interval, U);
+title('Controllo ottimo');
+legend('u1', 'u2', 'u3', 'u4', 'u5');
+figure(2)
+subplot(3, 1, 1);
+plot(interval, X(1,:), interval, Z(1,:));
+title('Componente 1');
+legend('x1', 'z1');
+subplot(3, 1, 2);
+plot(interval, X(2,:), interval, Z(2,:));
+title('Componente 2');
+legend('x2', 'z2');
+subplot(3, 1, 3);
+plot(interval, X(3,:), interval, Z(3,:));
+title('Componente 3');
+legend('x3', 'z3');
--- mylqt.m
+++ mylqt.m
@@ -1,37 +1,36 @@
-
 function [Kp ,Ki, g] = mylqt(A, B, C, Q, Qf, R, T, Z, X0)
+
+    % Calcola la componente proporzionale e quella integrale per il
+    % controllo ottimo.
+
+    % Matrici ausiliarie:
+    E = B * R^(-1) * B';
+    W = C' * Q;
+    V = C' * Q * C;
     
-    %Inizializziamo:
-    E = B*R^(-1)*B';
-    W = C'*Q;
-    V = C'*Q*C;
-    
-    P = zeros(size(C,2),size(C,2), T);
+    % Inizializzazione matrici:
+    P = zeros(size(C,2), size(C,2), T);
     I = eye(size(E,1), size(P,2));
     g = zeros(size(W,1), T);
-    Kp = zeros(size(B,2),size(X0,1), T);
-    Ki = zeros(size(B,2),size(g,1), T);
+    Kp = zeros(size(B,2), size(X0,1), T);
+    Ki = zeros(size(B,2), size(g,1), T);
     
-    P(:,:,T) = C'*Qf*C;
-    g(:,T) = C'*Qf*Z(:,T);
+    % Valori finali:
+    P(:,:,T) = C' * Qf * C;
+    g(:,T) = C' * Qf * Z(:,T);
     
-    %Fase backward:
+    % Calcolo del termine P (equazione di Riccati):
     for i = T-1:-1:1
-       
-        P(:,:,i) = A'*P(:,:,i+1)*(I + E* P(:,:,i+1) )^(-1)*A + V;
-        
+        P(:,:,i) = A' * P(:,:,i+1) * (I + E * P(:,:,i+1) )^(-1) * A + V;
     end
-    %Calcolo dell'errore integrale (fase backward):
+    % Calcolo del termine integrale g:
     for i = T-1:-1:1
-       
-        g(:,i) = A'*(I - (P(:,:,i+1)^(-1) + E )^(-1)*E)*g(:,i+1)+ W*Z(:,i);
-        
+        g(:,i) = A' * (I - (P(:,:,i+1)^(-1) + E )^(-1) * E) * g(:,i+1)+ W * Z(:,i);
     end
-    %Fase forward:
+    % Calcolo di Kp e Kg:
     for i = 1:T-1
-       
-        Kp(:,:,i) = (R + B'*P(:,:,i+1)*B)^(-1)*B'*P(:,:,i+1)*A;
-        Ki(:,:,i) = (R + B'*P(:,:,i+1)*B)^(-1)*B';
+        Kp(:,:,i) = (R + B' * P(:,:,i+1) * B)^(-1) * B' * P(:,:,i+1) * A;
+        Ki(:,:,i) = (R + B' * P(:,:,i+1) * B)^(-1) * B';
     end
     
 end
